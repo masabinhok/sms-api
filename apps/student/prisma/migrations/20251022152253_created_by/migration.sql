4843d3252c8021e4c1dd6dda202c3480d58097b5
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-10-22 21:15:31 +0545

    feat: add createdBy field to Student and Teacher models with corresponding migration

---
+++ apps/student/prisma/migrations/20251022152253_created_by/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `createdBy` to the `Student` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Student" ADD COLUMN     "createdBy" TEXT NOT NULL;
